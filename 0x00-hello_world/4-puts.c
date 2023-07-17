f9984f1c85c9798ee626180595ab23a2603f4a54
ekperi <georgegoldman2014@gmail.com>
2023-07-17 19:19:26 +0100

    fixed

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+/**
+* main - This is a description
+* print Programming is like building a multilingual puzzle
+* Return: int 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
