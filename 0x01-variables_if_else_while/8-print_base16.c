3f8889dad0f7b68f35678a81959d7c3a3324f019
ekperi <georgegoldman2014@gmail.com>
2023-07-19 13:02:37 +0100

    made ./8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: int
+ */
+int main(void)
+{
+	char alph = 'a'
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	while (alph < 'g')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+}
