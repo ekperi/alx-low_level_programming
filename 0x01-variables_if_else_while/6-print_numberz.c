7b8fa564a7a379fe3c8349030426b0ce668798c0
ekperi <georgegoldman2014@gmail.com>
2023-07-19 11:23:02 +0100

    created 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: int 0
+*/
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+putchar(num + '0');
+num++;
+}
+putchar('\n');
+return (0);
+}
