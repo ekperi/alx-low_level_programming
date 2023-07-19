2f45ea365da75cea766252594b47a4947a6f1f15
ekperi <georgegoldman2014@gmail.com>
2023-07-19 05:55:39 +0100

    fixed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: int
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
