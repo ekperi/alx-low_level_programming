f5930a8f3b236d22eea11f06a124a98097f9e9fa
ekperi <georgegoldman2014@gmail.com>
2023-07-19 12:29:48 +0100

    made 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: int
+ */
+int main(void)
+{
+	char alph = 'z';
+
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
