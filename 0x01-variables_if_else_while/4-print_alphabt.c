080a54197e67510f2a4492273bee4810701bb28a
ekperi <georgegoldman2014@gmail.com>
2023-07-19 05:46:16 +0100

    fixed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Return: int 0
+*/
+int main(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q' || alphabet == 'e')
+{
+continue;
+}
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
