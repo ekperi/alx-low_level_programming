70fe828ae6904524e39253d78f4c46682414338d
ekperi <georgegoldman2014@gmail.com>
2023-07-19 13:41:46 +0100

    fixed

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,37 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
 /**
-* main - compute to get random signed number
-* Return: int 0
-*/
+ * main - Entry point
+ *
+ * Description: print the last digit of the numbers stored in the variable n
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-int n;
+	int n;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (n % 10 > 5)
-{
-printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
-}
-else if (n % 10 == 0)
-{
-printf("Last digit of %i is %i and is 0\n", n, n % 10);
-}
-else if (n % 10 < 6 && n % 10 != 0)
-{
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
-}
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if  (n % 10 > 5)
+	{
+		printf("Last digit of %i is %i and is greater than 5\n", n, n % 10);
+	}
+
+	else if (n % 10 == 0)
+	{
+		printf("Last digit of %i is %i and is 0\n", n, n % 10);
+	}
+
+	else
+	{
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, n % 10);
+
+	}
+
+	return (0);
 }
