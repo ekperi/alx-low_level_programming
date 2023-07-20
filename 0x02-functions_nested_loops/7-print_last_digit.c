5387d3c564b156dd59a124f5bd00d0a3b08b30a1
ekperi <georgegoldman2014@gmail.com>
2023-07-20 10:11:53 +0100

    fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - get the last digit of a value
- * @n: int
- * Return: int
+ * print_last_digit- Entry point
+ *
+ * @i: integer parameter
+ *
+ * Description: Check modulus of integer
+ *
+ * Return: Always last digit of int
  */
-int print_last_digit(int n)
-{
-	int last_digit;
 
-	if (n < 0)
-		n *= -1;
-	last_digit = n % 10;
-	_putchar(last_digit + '0');
-	return (last_digit);
+int print_last_digit(int i)
+{
+	int ret_val;
+
+	if (i < 0)
+		ret_val = -1 * (i % 10);
+	else
+		ret_val = i % 10;
+	_putchar(ret_val + '0');
+	return (ret_val);
 }
