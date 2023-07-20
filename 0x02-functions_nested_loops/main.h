7e91c75a41596445079e2269d56f13dacb68d1f7
ekperi <georgegoldman2014@gmail.com>
2023-07-20 10:07:33 +0100

    git fixed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef HEADER_FILE
-#define HEADER_FILE
+#define HEADER_FIL
+
 
 int _putchar(char);
 void print_alphabet(void);
@@ -8,6 +9,12 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int)
+int print_last_digit(int);
 void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
 #endif
